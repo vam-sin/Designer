2697b335ac1dc826cf8cbf6db636684346286ad4
Vamsi Nallapareddy <vamsinallapareddy@gmail.com>
2020-06-03 13:37:16 +0530

    data explore

--- src/data_explore.py
+++ src/data_explore.py
@@ -1,22 +1,113 @@
 import h5py
-import numpy as np
-from PIL import Image
+import os
 import io
+from torch.utils.data import Dataset, DataLoader
+import h5py
+import numpy as np
+import pdb
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import pdb
+import torch.nn.functional as F
 
-filename = "../ds/flowers.hdf5"
+class Text2ImageDataset(Dataset):
 
-f = h5py.File(filename, 'r')
+    def __init__(self, datasetFile, transform=None, split=0):
+        self.datasetFile = datasetFile
+        self.transform = transform
+        self.dataset = None
+        self.dataset_keys = None
+        self.split = 'train' if split == 0 else 'valid' if split == 1 else 'test'
+        self.h5py2int = lambda x: int(np.array(x))
 
-dataset_keys = [str(k) for k in f['train'].keys()]
+    def __len__(self):
+        f = h5py.File(self.datasetFile, 'r')
+        self.dataset_keys = [str(k) for k in f[self.split].keys()]
+        length = len(f[self.split])
+        f.close()
 
-example_name = dataset_keys[0]
+        return length
 
-example = f['train'][example_name]
+    def __getitem__(self, idx):
+        if self.dataset is None:
+            self.dataset = h5py.File(self.datasetFile, mode='r')
+            self.dataset_keys = [str(k) for k in self.dataset[self.split].keys()]
 
-right_image = bytes(np.array(example['img']))
+        example_name = self.dataset_keys[idx]
+        example = self.dataset[self.split][example_name]
 
-right_image = Image.open(io.BytesIO(right_image)).resize((64, 64))
+        # pdb.set_trace()
 
-right_image.show()
+        right_image = bytes(np.array(example['img']))
+        right_embed = np.array(example['embeddings'], dtype=float)
 
-print(right_image)
+        example_name = self.dataset_keys[idx]
+        example = self.dataset[self.split][example_name]
+
+        # pdb.set_trace()
+
+        wrong_image, wrong_text = self.find_wrong_image(example['class'])
+        wrong_text = np.array(wrong_text).astype(str)
+
+        # pdb.set_trace()
+
+        wrong_image = bytes(np.array(example['img']))
+
+        right_image = Image.open(io.BytesIO(right_image)).resize((64, 64))
+        wrong_image = Image.open(io.BytesIO(wrong_image)).resize((64, 64))
+
+        right_image = self.validate_image(right_image)
+        wrong_image = self.validate_image(wrong_image)
+
+        txt = np.array(example['txt']).astype(str)
+
+        sample = {
+                'right_images': torch.FloatTensor(right_image),
+                'right_embed': torch.FloatTensor(right_embed),
+                'wrong_images': torch.FloatTensor(wrong_image),
+                'wrong_text': str(wrong_text),
+                'txt': str(txt)
+                 }
+
+        sample['right_images'] = sample['right_images'].sub_(127.5).div_(127.5)
+        sample['wrong_images'] =sample['wrong_images'].sub_(127.5).div_(127.5)
+
+        return sample
+
+    def find_wrong_image(self, category):
+        idx = np.random.randint(len(self.dataset_keys))
+        example_name = self.dataset_keys[idx]
+        example = self.dataset[self.split][example_name]
+        _category = example['class']
+
+        if _category != category:
+        	print(idx)
+        	return example['img'], example['txt']
+
+        return self.find_wrong_image(category)
+
+    def validate_image(self, img):
+        img = np.array(img, dtype=float)
+        if len(img.shape) < 3:
+            rgb = np.empty((64, 64, 3), dtype=np.float32)
+            rgb[:, :, 0] = img
+            rgb[:, :, 1] = img
+            rgb[:, :, 2] = img
+            img = rgb
+
+        return img.transpose(2, 0, 1)
+
+if __name__ == '__main__':
+	
+	filename = "../ds/flowers.hdf5"
+	f = h5py.File(filename, 'r')
+
+	ds = Text2ImageDataset(filename)
+
+	sample = ds.__getitem__(0)
+
+	print(sample)
+
+	print(ds.__len__)
+
